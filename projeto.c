a8aec0c57338a5f708389eeb3757b836985e4f78
Beloin Sena <beloin.rodrigues@gmail.com>
2023-11-16 22:39:39 -0300

    WIP

--- projeto.c
+++ projeto.c
@@ -1,26 +1,27 @@
-#include "16F874.h"
-
+#include <16f874A.h>
 #device adc=8
-#use delay(clock=10000000)
 
+#use delay(clock=1000000)
 #fuses XT,NOWDT,NOPUT
 
 
-void main() {
-   int i = 0;
-   long int tensao[5];
-   long int media_tensao;
-   char media;
+void main(){
+	int ref = 127;
+	float uk_1 = 0, ek = 0, ek_1 = 0, yk = 0;
+	char out;
 
-   setup_adc_ports(ALL_ANALOG);
-   setup_adc(ADC_CLOCK_INTERNAL);
-   set_adc_channel(0); // read_adc();
-
-   delay_us(100);
-   output_high(PIN_C0);
-
-   while(1) {
-
-   }
+	setup_adc_ports(ALl_ANALOG);
+	setup_adc(ADC_CLOCK_INTERNAL);
+	set_adc_channel(0); // or 1
+	delay_ms(1)
+	setup_timer_2(T2_DIV_BY_4, 65, 1);
+	setup_ccp1(CCP_PWM);
+	set_pwm1_duty(127);
+	enable_interrupts(GLOBAL | INT_TIMER2);
+	
+	while (1) {
+		delay_ms(1);
+		yk = read_adc
+	}
 
 }
